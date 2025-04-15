6a498b781a71ca95d494c6b4d820ad7a5ccca0c7
andrey.zebin <andrey.zebin@gmail.com>
2025-04-15 15:15:36 +0300

    init

--- lib/src/main/java/io/github/zebin/javabash/sandbox/AllFileManager.java
+++ lib/src/main/java/io/github/zebin/javabash/sandbox/AllFileManager.java
@@ -2,4 +2,11 @@ package io.github.zebin.javabash.sandbox;
 
 public interface AllFileManager extends DirWalker, DirChanger, DirViewer, BashPower {
 
+    default boolean remove(PosixPath path) {
+        return this.dirExists(path) ? this.removeDir(path) : this.removeFile(path);
+    }
+
+    default boolean exists(PosixPath path) {
+        return this.fileExists(path) || this.dirExists(path);
+    }
 }
